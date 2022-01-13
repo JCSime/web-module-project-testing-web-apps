6c0adb849fae0aae749f9f787aa061219806e4a3
John S <johnc.sime@gmail.com>
2022-01-13 16:39:26 -0500

    Created Understanding-questions.md and entered a list of things I could test. Created the first 5 test in ContactForm.js successfully.

---
+++ src/Understanding-questions.md
@@ -0,0 +1,6 @@
+* Check if form rendered.
+* Check if form validation is working.
+* Check if form validation allows form to submit without message input.
+* Check if first name, last name, email, and message input box lets you type.
+* Check if submit button exist and clickable.
+* Check if form inputs clear out.
--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -6,27 +6,61 @@ import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
-    
+    render(<ContactForm/>);
 });
 
 test('renders the contact form header', ()=> {
-    
+    render(<ContactForm/>);
+    const header = screen.queryByText(/contact form/i);
+    expect(header).toBeInTheDocument();
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+    waitFor(async ()=> {
+        render(<ContactForm/>);
+        const firstName = screen.getByLabelText(/First Name*/i);
+        const submit = screen.queryByRole("button");
+        userEvent.type(firstName, "bad");
+        userEvent.click(submit);
+        expect(firstName).toThrow();
+    })
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+    waitFor(async ()=> {
+        render(<ContactForm/>);
+        const firstName = screen.getByLabelText(/First Name*/i);
+        const lastName = screen.getByLabelText(/Last Name*/i);
+        const email = screen.getByLabelText(/Email*/i);
+        const submit = screen.queryByRole("button");
+        userEvent.type(firstName, lastName, email, null);
+        userEvent.click(submit);
+        expect(firstName, lastName, email).toThrow();
+    })
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-    
+    waitFor(async ()=> {
+        render(<ContactForm/>);
+        const firstName = screen.getByLabelText(/First Name*/i);
+        const lastName = screen.getByLabelText(/Last Name*/i);
+        const email = screen.getByLabelText(/Email*/i);
+        userEvent.type(firstName, "Thomas");
+        userEvent.type(lastName, "Anderson");
+        expect(email).toThrow();
+    });
+
+    //Alternate solution:
+        // render(<ContactForm/>);
+        // const firstName = screen.getByLabelText(/First Name*/i);
+        // const lastName = screen.getByLabelText(/Last Name*/i);
+        // userEvent.type(firstName, "Thomas");
+        // userEvent.type(lastName, "Anderson");
+        // expect(screen.getByText(/Error:/i)).toBeTruthy();
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
+
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
